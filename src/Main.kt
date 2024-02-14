403284fdd8648e0a4dc4df9a1762d8576b4c3c18
Davit-M <87268065+DaviDM2005@users.noreply.github.com>
2024-02-14 19:32:59 +0400

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,4 @@
-import kotlin.math.PI  // Import PI for mathematical calculations
-import kotlin.properties.ReadOnlyProperty
+import kotlin.math.PI  
 
 abstract class Shape {
     abstract val area: Double
@@ -14,7 +13,7 @@ class Circle(val radius: Double) : Shape() {
         get() = 2 * PI * radius
 }
 
-class Rectangle(val width: Double, val height: Double) : Shape() {
+open class Rectangle(val width: Double, val height: Double) : Shape() {
     override val area: Double
         get() = width * height
 
@@ -22,28 +21,31 @@ class Rectangle(val width: Double, val height: Double) : Shape() {
         get() = 2 * (width + height)
 }
 
-class Square(val sideLength: Double) : Shape() {
+class Square(val sideLength: Double) : Rectangle(sideLength,sideLength) {
     override val area: Double
         get() = sideLength * sideLength
 
     override val perimeter: Double
-        get() = 2 * (sideLength + sideLength)
+        get() = 4 * (sideLength)
 }
 
 
 
 fun main() {
-    // Example usage:
+    // Circle area and perimeter
     val circle = Circle(5.0)
     println("Circle area: ${circle.area}")
     println("Circle perimeter: ${circle.perimeter}")
 
-
+    // Rectangle area and perimeter
     val Rectangle = Rectangle(3.4,5.4)
     println("Reactangle area: ${Rectangle.area}")
     println("Reactangle perimeter: ${Rectangle.perimeter}")
 
+    // Rectangle area and perimeter
     val Square = Square(3.4)
     println("Square area: ${Square.area}")
     println("Square perimeter: ${Square.perimeter}")
 }
+
+
